b30592b33e5d570927131c43705917b2911b2a94
sri-218 <151498937+sri-218@users.noreply.github.com>
2026-01-10 18:49:31 +0530

    added testbench to verify datapath add and shift operations
    Implemented a testbench environment to validate the functionality of the datapath module.

--- datapath/testbench.v
+++ datapath/testbench.v
@@ -1,5 +1,3 @@
-// Code your testbench here
-// or browse Examples
 module testbench;
 
   reg clk, rst, ld_regs, add_en, shift_en;
@@ -7,7 +5,6 @@ module testbench;
   wire q0;
   wire [7:0] product_out;
 
-  // Connect your Datapath
   datapath dut (
     .clk(clk),
     .rst(rst),
@@ -20,45 +17,30 @@ module testbench;
     .product_out(product_out)
   );
 
-  // Create a Clock
   initial begin
     clk = 0;
     forever #5 clk = ~clk; 
   end
 
-  // Test Sequence
   initial begin
-    // --- THIS IS THE CRITICAL FIX ---
-    $dumpfile("dump.vcd");       // Create the file
-    $dumpvars(0, testbench);     // Dump ALL variables inside 'testbench' module
-    // --------------------------------
-
-    // 1. Initialize
+    $dumpfile("dump.vcd");       
+    $dumpvars(0, testbench);     
     rst = 1; ld_regs = 0; add_en = 0; shift_en = 0;
     multiplier_in = 4'b1011;   
     multiplicand_in = 4'b0101; 
-    #15; // Wait a bit
-
-    // 2. Release Reset
+    #15;
     rst = 0;
     #15;
-
-    // 3. Load Registers
     ld_regs = 1;
     #10;
     ld_regs = 0; 
-    
-    // 4. Do an ADD
     add_en = 1;
     #10;
     add_en = 0;
-
-    // 5. Do a SHIFT
     shift_en = 1;
     #10;
     shift_en = 0;
-
-    #50; // Wait for waves to settle
+    #50;
     $finish;
   end
 endmodule
